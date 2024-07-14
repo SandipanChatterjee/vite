4c8c78bce61b954421d653deb56c5e7c05f8355a
sandipan <sandipan.chatterjee8@gmail.com>
2024-07-14 19:14:03 +0530

    feat: added offline pwa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,39 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: '/manifest.webmanifest',
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/api\.pexels\.com\//,
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "api-cache",
+            },
+          },
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "images-cache",
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": "/src",
